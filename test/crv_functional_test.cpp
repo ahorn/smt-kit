a6a5f4f13d45e15f4a780371e0dce38467559615
ahorn <alex.horn@cs.ox.ac.uk>
2014-01-02 21:14:02 +0000

    Add SV-COMP'13 Stateful benchmark as functional test

--- test/crv_functional_test.cpp
+++ test/crv_functional_test.cpp
@@ -96,3 +96,89 @@ TEST(CrvFunctionalTest, SatFib6)
   EXPECT_FALSE(crv::tracer().flip());
 }
 
+void stateful_t0(
+  crv::Mutex& mutex,
+  crv::External<int>& i,
+  crv::External<int>& j)
+{
+  mutex.lock();
+  i = i + 1;
+  mutex.unlock();
+
+  mutex.lock();
+  j = j + 1;
+  mutex.unlock();
+}
+
+void stateful_t1(
+  crv::Mutex& mutex,
+  crv::External<int>& i,
+  crv::External<int>& j)
+{
+  mutex.lock();
+  i = i + 5;
+  mutex.unlock();
+
+  mutex.lock();
+  j = j - 6;
+  mutex.unlock();
+}
+
+// Adapted from SV-COMP'13 benchmark:
+//  https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/stateful01_true.c
+TEST(CrvFunctionalTest, UnsatStateful)
+{
+  crv::tracer().reset();
+  crv::Encoder encoder;
+
+  crv::Mutex mutex;
+  crv::External<int> i(10), j(10);
+
+  bool error = false;
+  do
+  {
+    crv::Thread t0(stateful_t0, mutex, i, j);
+    crv::Thread t1(stateful_t1, mutex, i, j);
+
+    t0.join();
+    t1.join();
+
+    crv::tracer().add_assertion(i != 16 || j != 5);
+
+    if (!crv::tracer().assertions().empty() &&
+        smt::sat == encoder.check_assertions(crv::tracer()))
+      error = true;
+  }
+  while (crv::tracer().flip());
+  EXPECT_FALSE(error);
+}
+
+// Adapted from SV-COMP'13 benchmark:
+//  https://svn.sosy-lab.org/software/sv-benchmarks/trunk/c/pthread/stateful01_false-unreach-label.c
+TEST(CrvFunctionalTest, SatStateful)
+{
+  crv::tracer().reset();
+  crv::Encoder encoder;
+
+  crv::Mutex mutex;
+  crv::External<int> i(10), j(10);
+
+  bool error = false;
+  do
+  {
+    crv::Thread t0(stateful_t0, mutex, i, j);
+    crv::Thread t1(stateful_t1, mutex, i, j);
+
+    t0.join();
+    t1.join();
+
+    crv::tracer().add_assertion(i == 16 && j == 5);
+
+    if (!crv::tracer().assertions().empty() &&
+        smt::sat == encoder.check_assertions(crv::tracer()))
+      error = true;
+  }
+  while (crv::tracer().flip());
+  EXPECT_TRUE(error);
+}
+
