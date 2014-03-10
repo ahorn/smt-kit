fb584e1d56e4e6c846b7e666e9fda63d8e2d3ca0
ahorn <alex.horn@cs.ox.ac.uk>
2014-03-10 20:13:17 +0000

    Add sequential symbolic modulus performance test
    This performance test illustrates the strengths and weaknesses
    of symbolic execution with a depth-first search strategy. Note
    that the measurements are about sequential code analysis.

--- test/crv_performance_test.cpp
+++ test/crv_performance_test.cpp
@@ -70,3 +70,74 @@ TEST(CrvPerformanceTest, PruneBranchesWithConstantPropagation)
   EXPECT_TRUE(sec.count() < 1);
 }
 
+TEST(CrvPerformanceTest, SafeSequentialSymbolicModulus)
+{
+  constexpr unsigned N = 64;
+  constexpr unsigned K = 7;
+  auto start = std::chrono::system_clock::now();
+
+  crv::tracer().reset();
+  crv::Encoder encoder; 
+
+  do
+  {
+    crv::External<unsigned> any_int;
+    crv::Internal<unsigned> k = any_int;
+
+    crv::tracer().add_assertion(0 <= k && k < K);
+    for (crv::Internal<unsigned> n = 0; crv::tracer().decide_flip(n < N); n = n + 1)
+    {
+      k = k + 1;
+      if (crv::tracer().decide_flip(k == K))
+      {
+        k = 0;
+      }
+    }
+
+    // expect k to be always strictly less than K
+    EXPECT_EQ(smt::unsat, encoder.check(!(k < K), crv::tracer()));
+  }
+  while (crv::tracer().flip());
+
+  auto end = std::chrono::system_clock::now();
+  std::chrono::seconds sec = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+  EXPECT_TRUE(sec.count() < 1);
+}
+
+// depth-first search finds bug very quickly, so large N is OK
+TEST(CrvPerformanceTest, UnsafeSequentialSymbolicModulus)
+{
+  constexpr unsigned N = 1024;
+  constexpr unsigned K = 7;
+  auto start = std::chrono::system_clock::now();
+
+  crv::tracer().reset();
+  crv::Encoder encoder; 
+
+  bool found_bug = false;
+  do
+  {
+    crv::External<unsigned> any_int;
+    crv::Internal<unsigned> k = any_int;
+
+    crv::tracer().add_assertion(0 <= k && k < K);
+    for (crv::Internal<unsigned> n = 0; crv::tracer().decide_flip(n < N); n = n + 1)
+    {
+      if (crv::tracer().decide_flip(k == K))
+      {
+        k = 0;
+      }
+      k = k + 1;
+    }
+
+    // there exists at least one path such that
+    // k fails to be strictly less than K
+    found_bug |= smt::unsat == encoder.check(!(k < K), crv::tracer());
+  }
+  while (!found_bug && crv::tracer().flip());
+  EXPECT_TRUE(found_bug);
+
+  auto end = std::chrono::system_clock::now();
+  std::chrono::seconds sec = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+  EXPECT_TRUE(sec.count() < 1);
+}
